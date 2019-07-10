6f9180d5b5c0236ac808c26d66d2608ce61e3738
akinnae <36315296+akinnae@users.noreply.github.com>
2019-07-10 13:22:57 -0400

    connect to PRcommenter, now sends comments

--- interface.py
+++ interface.py
@@ -27,9 +27,9 @@ def notes():
 @app.route('/home-sc', methods=['POST'])
 def send_comment():
     repo = request.form['send_comment_button']                                  # gets repo name from value of send_comment_button button
-    cur.execute("SELECT * FROM duppr_pair WHERE repo=%s", (repo,))        # gets pr number
-    row = cur.fetchall()
-    pr = row[2]
+    cur.execute("SELECT pr1 FROM duppr_pair WHERE repo=%s", (repo,))            # gets pr number
+    pr = cur.fetchall()
+    pr = int(pr[0][0], 10)                                                      # type as int
     PRcommenter.make_github_comment(repo, pr, "")                               # sends comment
     cur.execute("UPDATE duppr_pair SET comment_sent=1 WHERE repo=%s", (repo,))  # changes comment_sent value to 1 -- flags as sent
     conn.commit()                                                               # saves changes
